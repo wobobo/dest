938f8f2587a1fca8f4ba122532bd954dfb6ce4cc
wobobo <wobobo@nah.com>
2017-07-21 21:01:56 +0200

    my first changes

--- file1.hpp
+++ file1.hpp
@@ -5,6 +5,7 @@ class file1
 {
 public:
     file1() = default;
+    ~file1() = default;
 };
 
 #endif
---
+++ file3.hpp
@@ -0,0 +1,6 @@
+#ifndef FILE3_HPP
+#define FILE3_HPP
+
+inline int file3(int x, int y) { return x+y; }
+
+#endif
