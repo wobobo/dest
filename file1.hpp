3d99a727ea9f9b103953205204372f6d170383d4
yinifet <yinifet@nah.com>
2017-07-21 20:56:02 +0200

    some stuff

---
+++ file1.hpp
@@ -0,0 +1,10 @@
+#ifndef FILE1_HPP
+#define FILE1_HPP
+
+class file1
+{
+public:
+    file1() = default;
+};
+
+#endif
---
+++ file2.hpp
@@ -0,0 +1,10 @@
+#ifndef FILE2_HPP
+#define FILE2_HPP
+
+class file2
+{
+public:
+    file2() = default;
+};
+
+#endif
